c6c481dd80c5984a6cd799e8181a2d206a781153
AnitaNamjoon <anitachepkemoi1@gmail.com>
2023-09-22 09:57:18 +0300

    New commit

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,18 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.text
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        try:
+            json_data = json.loads(response_body)
+            return json_data
+        except ValueError:
+            print("Error: Response body is not valid JSON.")
+            return None
